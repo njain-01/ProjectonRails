03c79ca48fad0ebca2e6be94081b32468219d6db
njain-01 <nj3510@gmail.com>
2019-12-21 02:29:04 +0530

    new features

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,8 +3,22 @@ class ArticlesController < ApplicationController
         @articles = Article.all
       end
     def new
+        @article = Article.new
     end
     def show
         @article = Article.find(params[:id])
     end
+    def create
+        @article = Article.new
+        @article.title = params[:article][:title]
+        @article.body = params[:article][:body]
+        @article.save
+        redirect_to article_path(@article)
+      end
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to "/articles"
+    end
+
 end
